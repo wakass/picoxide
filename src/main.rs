7cb91a8f4dda25b9d6cfac2be07b0cf7c42e0386
WakA <alaeca@gmail.com>
2024-12-31 12:16:18 +0100

    rp2350: pico2w, first running blinky. gpio25 pin is not availabe (GPIO25 is connected to the wifi chip, the onboard LED can only be driven through this chip and requires SPI to drive)

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,59 @@
 use crate::prelude::*;
 use cortex_m_rt::entry;
 use panic_halt as _; //Ensure we have a panic handler
+use rp235x_hal as hal;
+
+use embedded_hal::delay::DelayNs;
+use embedded_hal::digital::OutputPin;
 
 mod error;
 mod prelude;
 mod utils;
 
-#[entry]
+#[link_section = ".start_block"]
+#[used]
+pub static IMAGE_DEF: hal::block::ImageDef = hal::block::ImageDef::secure_exe();
+
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+const WL_GPIO0: u32 = 32;
+
+#[hal::entry]
 fn main() -> ! {
-    let somevar = 1;
-    let start_dir = "./";
-    loop {}
+    let mut pac = hal::pac::Peripherals::take().unwrap();
+
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    // Configure the clocks
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .unwrap();
+
+    let mut timer = hal::Timer::new_timer0(pac.TIMER0, &mut pac.RESETS, &clocks);
+
+    // The single-cycle I/O block controls our GPIO pins
+    let sio = hal::Sio::new(pac.SIO);
+
+    // Set the pins to their default state
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    // Configure GPIO25 as an output
+    let mut led_pin = pins.gpio22.into_push_pull_output();
+    loop {
+        led_pin.set_high().unwrap();
+        timer.delay_ms(500);
+        led_pin.set_low().unwrap();
+        timer.delay_ms(500);
+    }
 }
